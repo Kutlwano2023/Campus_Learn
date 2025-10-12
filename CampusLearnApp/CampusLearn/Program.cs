91bb96ad4165bc727daf063b5d3719cbaac6749d
Reagile Motsepe <reagilemotsepe@gmail.com>
2025-10-12 18:48:41 +0200

    Updated Chat Hub

--- CampusLearnApp/CampusLearn/Program.cs
+++ CampusLearnApp/CampusLearn/Program.cs
@@ -1,72 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
 using CampusLearn.Data;
 using CampusLearn.Hubs;
 using CampusLearn.Services;
-using CampusLearn.Models;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-builder.Services.AddScoped<IChatService, ChatService>();
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
-builder.Services.AddIdentity<Users, IdentityRole>(options =>
-{
-options.Password.RequireNonAlphanumeric=false;
-options.Password.RequiredLength = 8;
-options.Password.RequireUppercase=false;
-options.Password.RequireLowercase=false;
-options.User.RequireUniqueEmail=true;
-options.SignIn.RequireConfirmedAccount = false;
-options.SignIn.RequireConfirmedEmail=false;
-
-
-
-})
-  .AddEntityFrameworkStores< AppDbContext>()
-  .AddDefaultTokenProviders();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-// Add PostgreSQL
+// ✅ Use PostgreSQL
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
 
-// Add SignalR
+// Other service registrations
+builder.Services.AddScoped<IChatService, ChatService>();
 builder.Services.AddSignalR();
 
-// Middleware
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
-
-// Enable WebSockets
 app.UseWebSockets();
 
-// Map routes and hubs
+// Map routes and SignalR hub
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapHub<ChatHub>("/chathub");
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Account}/{action=Login}/{id?}");
 
 app.Run();
